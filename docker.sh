68c52a6de3989e6914c0fffb21d1d870d8a30577
Steven Zemelka <steven.zemelka@gmail.com>
2020-09-16 00:48:55 +0200

    docker-sync support and php 7.4

--- docker.sh
+++ docker.sh
@@ -2,3 +2,16 @@ if [[ "$1" == "ssh" ]]
    then
       bash -c "clear && docker exec -it klizzy_php zsh"
    fi
+if [[ "$1" == "start" ]]
+   then
+      docker-sync start && docker-compose -f docker-compose.yml up -d
+fi
+if [[ "$1" == "stop" ]]
+   then
+      docker-sync stop && docker-compose stop
+fi
+
+#if [[ "$1" == "setup" ]]
+#	then
+#
+#fi
