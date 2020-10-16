4fd4537b421645d485c3d7445ca2f17c4030ac1a
Steven Zemelka <steven.zemelka@gmail.com>
2020-10-16 13:58:49 +0200

    docker linux support

--- docker.sh
+++ docker.sh
@@ -1,9 +1,21 @@
+case "$(uname -s)" in
+  Darwin)
+    ENV_MAC=true
+    ;;
+  *)
+    ENV_MAC=false
+   ;;
+esac
+
 if [[ "$1" == "ssh" ]]
    then
       bash -c "clear && docker exec -it klizzy_php zsh"
    fi
 if [[ "$1" == "start" ]]
    then
+         if [[ ${ENV_MAC} == false ]]; then
+        docker-compose -f docker-compose.yml up -d
+    fi
       docker-sync start && docker-compose -f docker-compose.yml up -d
 fi
 if [[ "$1" == "stop" ]]
